dc6b84340d49c3e25bd357241756dbedf9aee180
lps08 <lucianolps08@gmail.com>
2019-11-26 13:01:50 -0300

    Atualização do bando de dados de acordo com as modificações e inserções feitas

--- site/_bando_de_dados/ceu.sql
+++ site/_bando_de_dados/ceu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Nov-2019 às 17:11
+-- Tempo de geração: 26-Nov-2019 às 17:00
 -- Versão do servidor: 10.4.8-MariaDB
 -- versão do PHP: 7.3.11
 
@@ -27,8 +27,8 @@ SET time_zone = "+00:00";
 --
 -- Estrutura da tabela `tb_evento`
 --
--- Criação: 19-Nov-2019 às 14:54
--- Última actualização: 19-Nov-2019 às 16:04
+-- Criação: 26-Nov-2019 às 11:42
+-- Última actualização: 26-Nov-2019 às 15:49
 --
 
 CREATE TABLE `tb_evento` (
@@ -37,6 +37,7 @@ CREATE TABLE `tb_evento` (
   `nome` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `descricao` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `area` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `preco_evento` float(5,2) NOT NULL DEFAULT 0.00,
   `qntd_part` int(11) NOT NULL,
   `data_inicio` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -44,7 +45,9 @@ CREATE TABLE `tb_evento` (
   `estado` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `cidade` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `cep` int(11) NOT NULL,
-  `data_cadastro` datetime NOT NULL DEFAULT current_timestamp()
+  `data_cadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `num_usuario_cads` int(11) NOT NULL DEFAULT 0,
+  `usuario_cads` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -57,12 +60,9 @@ CREATE TABLE `tb_evento` (
 -- Extraindo dados da tabela `tb_evento`
 --
 
-INSERT INTO `tb_evento` (`id`, `id_usuario`, `nome`, `email`, `descricao`, `preco_evento`, `qntd_part`, `data_inicio`, `data_fim`, `estado`, `cidade`, `cep`, `data_cadastro`) VALUES
-(3, 1, 'tesste', 'lucianolps08@gmail.com', 'ljdcçolaskjdçaljkdlaçkjdçlkasjdaksdnhjwkwiljudqwoçopidjkqmdod okjqpokiqopkqop', 0.00, 20, '12/11/2019', '16/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-19 12:00:21'),
-(4, 1, 'bomba', 'lucianolps08@gmail.com', 'uma bomba', 0.00, 100, '20/11/2019', '23/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-19 12:01:15'),
-(8, 1, 'carroTunado', 'lucianolps08@gmail.com', 'um carro', 100.00, 10, '14/11/2019', '15/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-19 12:08:10'),
-(9, 1, 'carroHumilde', 'lucianolps08@gmail.com', 'um carro', 0.00, 10, '14/11/2019', '15/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-19 12:08:51'),
-(11, 1, 'sprint', 'lucianolps08@gmail.com', 'Uma apresentação de uma sprint', 500.00, 20, '19/11/2019', '19/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-19 13:04:23');
+INSERT INTO `tb_evento` (`id`, `id_usuario`, `nome`, `email`, `descricao`, `area`, `preco_evento`, `qntd_part`, `data_inicio`, `data_fim`, `estado`, `cidade`, `cep`, `data_cadastro`, `num_usuario_cads`, `usuario_cads`) VALUES
+(5, 1, 'Teste Palestra', 'lucianolps08@gmail.com', 'Um teste de um evento de uma palestra', 'palestra', 10.00, 100, '24/11/2019', '25/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-24 23:33:40', 1, '10#'),
+(7, 1, 'Teste Minicurso', 'lucianolps08@gmail.com', 'Um teste de um evento de um Minicurso', 'minicurso', 2.00, 20, '24/11/2019', '30/11/2019', 'PI', 'Piripiri', 64260000, '2019-11-24 23:35:36', 1, '10#');
 
 -- --------------------------------------------------------
 
@@ -93,7 +93,9 @@ CREATE TABLE `tb_usuario` (
 
 INSERT INTO `tb_usuario` (`id`, `nome`, `usuario`, `email`, `senha`, `estado`, `cidade`, `cep`) VALUES
 (1, 'Luciano', 'lps08', 'lucianolps08@gmail.com', '1234', 'PI', 'Piripiri', 64260000),
-(4, 'teste1212', 'teste11', 'teste1212@teste.com', 'teste12', 'PI', 'Piripiri', 64260000);
+(8, 'Luciano', '123', 'lucianolps07@gmail.com', '1234', 'PI', 'Piripiri', 64260),
+(9, 'Luciano', 'lps18', 'lucianolps18@gmail.com', '1234', 'PI', 'Piripiri', 64260),
+(10, 'Luciano', 'lps10', 'lucianolps10@gmail.com', '1234', 'PI', 'Piripiri', 64260000);
 
 --
 -- Índices para tabelas despejadas
@@ -120,13 +122,13 @@ ALTER TABLE `tb_usuario`
 -- AUTO_INCREMENT de tabela `tb_evento`
 --
 ALTER TABLE `tb_evento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restrições para despejos de tabelas
