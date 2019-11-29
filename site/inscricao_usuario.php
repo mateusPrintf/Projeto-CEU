923a8c56d62ec61113bb2f14eaf3a58a9e0403c4
lps08 <lucianolps08@gmail.com>
2019-11-29 15:19:01 -0300

    adicionado novo bd para inscrições

--- site/inscricao_usuario.php
+++ site/inscricao_usuario.php
@@ -11,43 +11,55 @@
 
         $conexao = new PDO($dsn, $user, $senha);
 
-        $query = "select usuario_cads, num_usuario_cads, qntd_part from tb_evento where id = :id";
-        $stmt = $conexao->prepare($query);
-        $stmt->bindValue(':id', $_GET['evento']);
-        $stmt->execute();
+        $queryQntPart = "select num_usuario_cads, qntd_part from tb_evento where id = :id";
+        $stmtQntPart = $conexao->prepare($queryQntPart);
+        $stmtQntPart->bindValue(':id', $_GET['evento']);
+        $stmtQntPart->execute();
 
-        $users = $stmt->fetch(PDO::FETCH_OBJ);
-        $listaUsers = explode("#", $users->usuario_cads);
+        $qntdEvento = $stmtQntPart->fetch(PDO::FETCH_OBJ);
 
-        if ($users->num_usuario_cads <= $users->qntd_part) {
-            foreach ($listaUsers as $id) {
-                if ($id == $_SESSION['id']) {
+        $queryInsc = "select * from tb_inscricoes where id_evento = :id";
+        $stmtInsc = $conexao->prepare($queryInsc);
+        $stmtInsc->bindValue(':id', $_GET['evento']);
+        $stmtInsc->execute();
+
+        $users = $stmtInsc->fetchAll(PDO::FETCH_OBJ);
+
+        echo '<pre>';
+        print_r($users);
+        echo '</pre>';
+
+        if ($qntdEvento->num_usuario_cads <= $qntdEvento->qntd_part) {
+            foreach ($users as $usr) {
+                if ($usr->id_usuario == $_SESSION['id']) {
                     //header('Location: evento.php?acao=jaCadastrado');
                     $jaCadastrado = true;
                 }
             }
     
             if (!$jaCadastrado) {
-                $query1 = "update tb_evento set usuario_cads = :id_user where id = :id";
+                $query1 = '
+                    insert into tb_inscricoes(
+                        id_evento, id_usuario
+                        ) values (
+                            :id_evento, :id_usuario
+                        )';
+
                 $stmt1 = $conexao->prepare($query1);
-                $stmt1->bindValue(':id_user', $users->usuario_cads . $_SESSION['id'] . '#');
-                $stmt1->bindValue(':id', $_GET['evento']);
-                $stmt1->execute();
+                $stmt1->bindValue(':id_evento', $_GET['evento']);
+                $stmt1->bindValue(':id_usuario', $_SESSION['id']);
+                echo $stmt1->execute();
         
-                $query2 = "select num_usuario_cads from tb_evento where id = :id";
-                $stmt2 = $conexao->prepare($query2);
-                $stmt2->bindValue(':id', $_GET['evento']);
-                $stmt2->execute();
-        
-                $numUser = $stmt2->fetch(PDO::FETCH_OBJ);
-        
-                $query3 = "update tb_evento set num_usuario_cads = :num_usuario_cads where id = :id";
+                $query3 = 'update tb_evento set num_usuario_cads = :num_usuario_cads where id = :id';
                 $stmt3 = $conexao->prepare($query3);
-                $stmt3->bindValue(':num_usuario_cads', $numUser->num_usuario_cads + 1);
+                $stmt3->bindValue(':num_usuario_cads', $qntdEvento->num_usuario_cads + 1);
                 $stmt3->bindValue(':id', $_GET['evento']);
-                $stmt3->execute();
+                echo $stmt3->execute();
     
                 header('Location: evento.php?acao=inscricaoFeita');
+
+            }else {
+                header('Location: evento.php?acao=jaCadastrado');
             }
 
         }else {
