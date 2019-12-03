303756886aa28572f873f998845c1042045092b0
Bernardo Junior <juniorxy29@hotmail.com>
2019-12-02 21:55:37 -0300

    Cupom de Desconto novo

---
+++ site/Pagina 2/modal_cupom.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./_css/style_menu.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+
+    <script>
+    $(function() {
+        $("#calendario").datepicker({
+            dateFormat: 'dd-mm-yy',
+            dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D']
+        });
+    });
+    </script>
+
+</head>
+
+<body>
+
+    <!-- Modal -->
+    <div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog"
+        aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header cupom" style="background: rgb(50, 150, 255)">
+                   <h4 class="modal-title" id="TituloModalCentralizado">Cupom de Desconto</h4> 
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="inputAddress">Código</label>
+                            <input type="text" style="text-transform:uppercase" class="form-control" id="inputAddress" placeholder="EX: #PIRI10">
+                        </div>
+                        <label>Desconto</label>
+                        <div class="row">
+                            <div class="input-group col">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">R$</span>
+                                </div>
+                                <input type="text" class="form-control">
+                            </div>
+                            <b>ou</b>
+                            <div class="input-group col">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">%</span>
+                                </div>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <form>
+                            <br>
+                            <div class="row">
+                                <div class="col">
+                                    <label>Quantidade</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col">
+                                    <label>Validade</label>
+                                    <input type='text' class="form-control" id="calendario" name="data_fim">
+                                </div>
+                            </div>
+                        </form>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+                    <button type="button" class="btn btn-primary">Confirmar</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
