5323a8a0f029fbe37c9220eca8b814433df7fde2
Bernardo Junior <juniorxy29@hotmail.com>
2019-12-10 11:22:05 -0300

    Tela de usuario para geração de certificados

---
+++ site/user.php
@@ -0,0 +1,92 @@
+<? require_once "validador_acesso.php"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>CEU Online</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="./_css/style_menu.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./_css/style_gerenciamento_usuario.css">
+    <link rel="stylesheet" href="./_css/style_menu.css">
+</head>
+
+<body>
+    <div class="menu-top menu-font">
+        <header class="cabecalho">
+            <div class="logo">
+                <a href="#inicio">
+                    <img src="./_imagens/CEU_noname_pequeno.png" alt="Logo" />
+                </a>
+            </div>
+            <button class="menu-toggle">
+              <i class="fa fa-lg fa-bars"></i>
+          </button>
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="#inicio">Início</a>
+                    </li>
+                    <li>
+                        <a href="evento.php">Eventos</a>
+                    </li>
+                    <li>
+                        <a href="#sobre">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#contato">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+            <aside class="autenticacao">
+                <a href="logoff.php">Sair</a>
+            </aside>
+
+        </header>
+    </div>
+
+    <div class="sidebar-container">
+        <div class="sidebar-logo">
+            Gerenciamento
+        </div>
+        <ul class="sidebar-navigation">
+            <li>
+                <a href="./gerar_certificado/gerador.php">
+                    <i class="fa fa-home" aria-hidden="true"></i> Certificados
+                </a>
+            </li>
+            <li>
+                <a href="./gerenciamento_user_evento.php">
+                    <i class="fa fa-tachometer" aria-hidden="true"></i> Eventos inscritos
+                </a>
+            </li>
+            <li>
+                <a href="./gerenciamento_user_evento_inscrito.php">
+                    <i class="fa fa-tachometer" aria-hidden="true"></i> 
+                </a>
+            </li>
+            
+        </ul>
+    </div>
+
+    <div class="content-container">
+
+        <div class="container-fluid">
+
+            <!-- Main component for a primary marketing message or call to action -->
+            <div class="jumbotron">
+                <h1>Area do Usuário</h1>
+            </div>
+
+            <div class="container-fluid">
+
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
